49e4d57a08b75c7b7179eaf7b09cb9dae3da793b
Suisoku <dev.ziani@gmail.com>
2018-04-05 07:34:05 +0200

    fff

--- DATABASE_SEMINAIRES/src/creationSem/Creneau.java
+++ DATABASE_SEMINAIRES/src/creationSem/Creneau.java
@@ -11,4 +11,23 @@ package creationSem;
  */
 public class Creneau {
     
+    private String moment;
+    
+    private String nom_act1;
+    private String nom_act2;
+    private String nom_act3;
+    
+    public void ajouterMoment(String mom){
+        this.moment = mom;
+    }
+    
+    public void ajouterAct(String act , int numero){
+        if(numero == 1)this.nom_act1 = act;
+        if(numero == 2)this.nom_act2 = act;
+        if(numero == 3)this.nom_act3 = act;
+    }
+    
+    public String getMoment(){
+        return this.moment;
+    }
 }
--- DATABASE_SEMINAIRES/src/interface_main/saisie_sem.java
+++ DATABASE_SEMINAIRES/src/interface_main/saisie_sem.java
@@ -5,6 +5,7 @@
  */
 package interface_main;
 
+import creationSem.Creneau;
 import creationSem.Seminaire;
 import java.util.Scanner;
 
@@ -19,6 +20,9 @@ public abstract class saisie_sem {
         Seminaire sem = new Seminaire();
         Scanner input = new Scanner(System.in);
         
+        /*** SAISIE DES DIFFERENTS DONNES CONERCERNANT UN SEMINAIRE ***/
+        /*************************************************************/
+        
         System.out.println("Veuillez saisir le theme:");
         sem.ajouterTheme(input.nextLine());
         
@@ -26,12 +30,47 @@ public abstract class saisie_sem {
         sem.ajouterAnimateur(input.nextInt());
         
         
-        System.out.println("Voulez vous un dejeuner quoi qu'il arrive ? OUI / * ");
+        System.out.println("Voulez vous un dejeuner quoi qu'il arrive ? O/N ");
         
-        if(input.nextLine() == "OUI"){
+        if(input.nextLine().equals("O")){
             sem.ajouterDejeuner(true);
         }
         else sem.ajouterDejeuner(false); // peut etre overrided en cas d'une journée complete
+        
+        System.out.println("Saisir votre premier creneau:");
+        System.out.println("Saisir le moment du creneau 1, Matin / Apresmidi [Matin|Aprem] :");
+        
+        Creneau cren1 = new Creneau();
+        Creneau cren2;
+        
+        cren1.ajouterMoment(input.nextLine());
+        
+        System.out.println("Nom activité 1:");
+        cren1.ajouterAct(input.nextLine(), 1);
+        
+        System.out.println("Nom activité 2:");
+        cren1.ajouterAct(input.nextLine(), 2);
+        
+        System.out.println("Nom activité 3:");
+        cren1.ajouterAct(input.nextLine(), 3);
+        
+        System.out.println("Voulez vous saisir un deuxieme creneau ? O|N :");
+        if(input.nextLine().equals("O")){
+            cren2 = new Creneau();
+            
+            cren2.ajouterMoment(input.nextLine());
+        
+            System.out.println("Nom activité 1:");
+            cren2.ajouterAct(input.nextLine(), 1);
+        
+            System.out.println("Nom activité 2:");
+            cren2.ajouterAct(input.nextLine(), 2);
+        
+            System.out.println("Nom activité 3:");
+            cren2.ajouterAct(input.nextLine(), 3);
+        }
+        
+        
         
         
         return sem;
